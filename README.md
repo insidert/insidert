5284cdac9436f4795625c11971f23c2cab71287a
Ravi Teja <raviteja155@gmail.com>
2023-02-01 23:30:24 +0530

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Inside Ravi Teja
+
+Hey! This is Ravi Teja. I am a full-stack developer and Entrepreneur. I use Laravel, Vue, Bootstrap and Flutter for my projects. This is my website & blog repo. 
+
+Built with Astro.
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Insidert';
+export const SITE_DESCRIPTION = 'Website and blog of Ravi Teja.';
